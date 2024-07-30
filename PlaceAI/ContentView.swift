162d8964d53bb1486585bf1aa79479b31e240e45
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2024-07-30 13:03:56 +0700

    Initial Commit

---
+++ PlaceAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlaceAI
+//
+//  Created by irfan wahendra on 30/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlaceAI/PlaceAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceAIApp.swift
+//  PlaceAI
+//
+//  Created by irfan wahendra on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaceAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
